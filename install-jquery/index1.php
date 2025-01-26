8d30d51e5fe1e1b547eb5b4fc9cb2cbbc3e1ed0e
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-26 15:57:13 +0700

    Membuat index php - Memcoba install dengan beberapa cara

---
+++ install-jquery/index1.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar JQuery</title>
+
+    <!-- jQuery dari folder local  -->
+    <script src="src/jquery.min.js"></script>
+
+    <!-- Uncompressed -->
+    <!-- <script src="https://code.jquery.com/jquery-3.7.1.js"></script> -->
+
+    <!-- Compressed -->
+    <!-- <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script> -->
+
+    <!-- install dengan CDN -->
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script> -->
+</head>
+
+<body>
+    <h1>Install dengan Uncompressed, compressed, CDN, dan local</h1>
+
+    <script>
+        $(document).ready(alert('Hello Word'));
+    </script>
+</body>
+
+</html>
